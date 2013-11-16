625e81370def306870c79eac8fb4efdab9fa77f8
Isaac White <iaw2105@columbia.edu>
2013-11-16 16:27:29 -0500

    Food lookup & distance calculations by calories. Beginning of event api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
+var HD2013 = {};
+HD2013.foodItemList = [];
 
+HD2013.getJSONObj = function (url, property) {
+	var response = $.get(url, function (data) {
+		var jsonObj = response.responseJSON;
+		var returnVal = jsonObj[property];
+		HD2013.sessionID = returnVal;
+		var requestVars = ["url","product_name"];
+		HD2013.getFoodInfo();
+	});
+}
+
+HD2013.getFoodInfo = function () {
+	var sessionID = HD2013.sessionID;
+	var url = "http://api.foodessentials.com/label_summary?u=016000264601&sid=" + HD2013.sessionID + " &appid=NYT_HackDay&f=json&api_key=6u2qj2wz3rxn769s3mcztz2e";
+	var response = $.get(url, function (data) {
+		var jsonObj = response.responseJSON;
+		var foodName = jsonObj.product_name;
+		var productUrl = jsonObj.url;
+
+		var html = $.get(productUrl, function (data) {
+			//data is the html response
+			HD2013.testData = data;
+			var calories = $(data).find("#label_sorting_title .center_text .right_text.tx_3")[0];
+			var calorieCount = parseInt($(calories).contents()[0]['data']);
+			HD2013.foodItemList.push( new HD2013.FoodItem(foodName,calorieCount,productUrl) );
+			// console.log(HD2013.foodItemList);
+		})
+	});
+}
+
+HD2013.FoodItem = function (name,calories,url) {
+	this.name = name;
+	this.calories = calories;
+	this.url = url;
+}
+
+HD2013.calculateDistance = function (calories,type) {
+	var distance;
+	if (type === "walk") {
+		distance = calories / (.57 * 160);
+	} else {
+		distance = calories / (.72 * 160);
+	}
+	return distance * 1609.34; //now it's in meters :)
+}
+
+HD2013.getJSONObj("http://api.foodessentials.com/createsession?uid=001&devid=001&appid=NYT_HackDay&f=json&api_key=6u2qj2wz3rxn769s3mcztz2e","session_id");
+
+HD2013.getEvents = function (distanceInMeters,startLat,startLng,start,end) {
+	var apiKey = "1f26f178792c1bc75bd269b3af192b86:7:56579220"
+	var dateRange;
+	var url = "http://api.nytimes.com/svc/events/v2/listings.json?";
+	if (start && end) {
+		dateRange = "&date_range" + start + "%3A" + end;
+		url += dateRange;
+	}
+	url += "&ll=" + startLat + "%2C" + startLng + "&radius=" + distanceInMeters + "&api-key=" + apiKey;
+
+	var response = $.get(url, function (data) {
+		var jsonObj = response.responseJSON;
+		console.log(jsonObj);
+	});
+}
